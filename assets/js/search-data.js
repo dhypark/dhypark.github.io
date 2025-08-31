321c430ece557716e9c426ed84e99ba3ce23d571
dhypark <dohyunpark22@gmail.com>
2025-08-31 12:22:29 +0000

    Deploying to gh-pages from @ dhypark/dhypark.github.io@777c223f49aae8d099f5a1dab46fa20ce253f106 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,11 +440,11 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-a-simple-inline-announcement",
-          title: 'A simple inline announcement.',
+            },},{id: "news-i-received-my-ph-d-from-pusan-natioanl-university-pnu",
+          title: 'I received my Ph.D. from Pusan Natioanl University (PNU).',
           description: "",
-          section: "News",},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
-          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          section: "News",},{id: "news-i-started-my-postdoctoral-research-at-pnu",
+          title: 'I started my postdoctoral research at PNU.',
           description: "",
           section: "News",},{id: "projects-project-1",
           title: 'project 1',
